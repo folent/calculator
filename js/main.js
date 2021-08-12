295ba202fad94cb67154eb19b84a1fed6f27d0c7
Vadim Aleshchyk <folent1@yandex.ru>
2021-08-12 15:10:41 +0300

    init project

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// const numberButtons__wrapper = document.querySelector('.number-buttons__wrapper');
+// const actionButtons__wrapper = document.querySelector('.action-buttons__wrapper');
+// const calculatorInput = document.getElementById('calculatorInput');
+
+// const numberButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, ',', '='];
+// const actionButtons = ['/', '*', '-', '+'];
+
+// const HTMLNumberButtons = numberButtons.map(el => {
+//     return `<div><button class='number-btn'>${el}</button></div>`
+// }).join('');
+
+// numberButtons__wrapper.innerHTML = HTMLNumberButtons;
+
+// const HTMLActionButtons = actionButtons.map(el => {
+//     return `<div><button class='action-btn'>${el}</button></div>`
+// }).join('');
+
+// actionButtons__wrapper.innerHTML = HTMLActionButtons;
+
